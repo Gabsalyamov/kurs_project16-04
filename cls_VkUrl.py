a26b24d3724d8c6604067a8c515f0baca4b7103e
Gabsalyamov <g-ars.ru@mail.ru>
2022-04-16 17:59:57 +0500

    First commit

---
+++ cls_VkUrl.py
@@ -0,0 +1,151 @@
+import requests
+from cls_HttpReq import HttpR
+
+"""
+This is the VKontakte API communication class
+"""
+
+
+class VkUrl(HttpR):
+    url_ = "https://api.vk.com/method/"
+
+    def __init__(self, token_file_n: str):
+        super().__init__(token_file_n)
+
+    def get_url(self, method: str):
+        """
+        This method just merge an default url and http method name.
+        """
+        return self.url_ + method
+
+    def get_params(self, fields: str, pdict: dict):
+        """
+        This method just merge http request parameters.
+        """
+        return {'access_token': self.token,
+                'v': '5.81',
+                'fields': fields} | pdict
+
+    def search_groups(self, gr_name: str, sorting=0) -> dict:
+        """
+        Gets data of vk groups by group name.
+        """
+        result = requests.get(self.get_url(method="groups.search"),
+                              params=self.get_params(
+                                  fields='',
+                                  pdict={'q': gr_name, 'sort': sorting, 'count': 10}),
+                              timeout=5)
+
+        print(result)
+
+        result = result.json()['response']['items']
+
+        return result
+
+    def search_groups_by_id(self, *list_) -> dict:
+        """
+        Gets data of vk groups by group id.
+        """
+        result = requests.get(self.get_url(method="groups.getById"),
+                              params=self.get_params(
+                                  fields='members_count,activity,description',
+                                  pdict={'group_ids': ','.join(str(idx) for idx in list_)}),
+                              timeout=5)
+
+        result = result.json()['response']
+
+        return result
+
+    def get_personal_data(self, user_id: str) -> dict:
+        """
+        Gets a user's data by user id
+        """
+        result = requests.get(self.get_url(method="users.get"),
+                              params=self.get_params(
+                                  fields='education,photo_400_orig,contacts',
+                                  pdict={'user_ids': user_id}),
+                              timeout=5)
+
+        return result.json()
+
+    def get_photo_f_profile(self, user_id: str, album_name: str) -> dict | str:
+
+        """
+        Gets a user's photos data by user id
+        """
+        result = requests.get(self.get_url(method="photos.get"),
+                              params=self.get_params(
+                                  fields='',
+                                  pdict={'owner_id': user_id,
+                                         'album_id': album_name,
+                                         'count': '200',
+                                         'photo_sizes': '1',
+                                         'extended': '1'}),
+                              timeout=5)
+
+        if result.status_code == 200 and 'error' not in result.json():
+            return result.json()['response']['items']
+        else:
+            return f"Error"
+
+    def search_albums(self, user_id: str) -> dict:
+        """
+        Gets albums list.
+        """
+        result = requests.get(self.get_url(method="photos.getAlbums"),
+                              params=self.get_params(
+                                  fields='',
+                                  pdict={'owner_id': user_id}),
+                              timeout=5)
+
+        result = result.json()
+
+        return result
+
+    @staticmethod
+    def format_files_list(photo_list: dict, qtt: int) -> list:
+        """
+        Format a list of files_inf_list by template:
+            [{
+            "file_name": "34.jpg",
+            "data": "data"
+            "url": url to download
+            "size": "z"
+            "width": width of photo
+            }]
+        Make a list of files (files_list) for next
+        """
+        files_inf_list = list()
+
+        for photo in photo_list:
+            # Choose the best resolution photo
+            max_photo = max(photo['sizes'], key=lambda size: int(size['width']))
+
+            file_name = f"{photo['likes']['count']}.jpeg"
+
+            # Collect the list of files.
+            files_inf_list.append({'file_name': file_name,
+                                   'likes': photo['likes']['count'],
+                                   'date': photo['date'],
+                                   'url': max_photo['url'],
+                                   'size': max_photo['type'],
+                                   'width': max_photo['width']})
+
+        files_inf_list.sort(key=lambda x: int(x['width']), reverse=True)
+
+        if len(files_inf_list) < qtt:
+            qtt = len(files_inf_list)
+
+        # print(f"qtt: {qtt}")
+        files_inf_list = [files_inf_list[i] for i in range(qtt)]
+
+        # Solve the file name conflict
+        tmp_list = list()
+        for file in files_inf_list:
+            new_file_name = file['file_name']
+            if new_file_name in tmp_list:
+                new_file_name = f"{file['likes']}_{file['date']}.jpeg"
+                file['file_name'] = new_file_name
+            tmp_list.append(new_file_name)
+
+        return files_inf_list
---
+++ main.py
@@ -0,0 +1,56 @@
+from cls_VkUrl import VkUrl
+from cls_YaUploader import YaUploader
+import file_exchange
+
+if __name__ == '__main__':
+
+    while True:
+        command = input("Please choose the social net: \n('v'-VKontakte, 'o' -Odnoklassniki, 'q'- to quit): \t")
+
+        match command:
+            case 'q':
+                break
+            case 'v':
+                print("This program takes VK token from file 'vk_token.txt'")
+                print("and Yandex token from file 'yd_token.txt'")
+
+                vk = VkUrl('vk_token.txt')
+
+                vk_id = input("Input vk id: \t")
+                album_name = input("Input a album name: \t")
+                photo_quant = int(input("Input a quantity of photos: \t"))
+                yd_path = input("Input a yandex disc directory to save files: \t")
+
+                # Get the photo list from user account.
+                photo_lst = vk.get_photo_f_profile(vk_id, album_name)
+
+                if photo_lst != "Error":
+                    # Download files Form the list of files.
+                    files_list = file_exchange.download_files(vk.format_files_list(photo_lst, photo_quant))
+
+                    # Upload files to Yandex disk.
+                    y_disc = YaUploader('yd_token.txt')
+                    y_disc.upload_files_from_local(files_list=files_list, yd_path=yd_path)
+
+                else:
+                    # Print "Error"
+                    print(photo_lst)
+
+            case 'o':
+                print("This program takes Odnoklassniki token from file 'ok_token.txt',")
+                print("Yandex token from file 'yd_token.txt'")
+                print("and other authorization data from file 'ok_data.txt'")
+                ok = OkUrl('ok_token.txt')
+
+                photo_quant = int(input("Input a quantity of photos: \t"))
+                yd_path = input("Input a yandex disc directory to save files: \t")
+                second_user = input("Input a Odnoklassniki user id: \t")
+
+                photo_lst = ok.format_files_list(ok.get_photo_f_profile(fid=second_user), photo_quant)
+
+                # Download files Form the list of files.
+                files_list = file_exchange.download_files(photo_lst)
+
+                # Upload files to Yandex disk.
+                y_disc = YaUploader('yd_token.txt')
+                y_disc.upload_files_from_local(files_list=files_list, yd_path=yd_path)
